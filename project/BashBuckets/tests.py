4283679e0a795aa4f036c9bd5e7d225d86b9f2e8
0xWra1th <danielscragg@outlook.com>
2021-06-05 15:35:16 +0200

    Initial Testing

--- project/BashBuckets/tests.py
+++ project/BashBuckets/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+class Endpoints(TestCase):
+
+    # Test that the index page is being served in response to GET
+    def testIndexGET(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    # Test that the analytics page is being served in response to GET
+    def testAnalyticsGET(self):
+        response = self.client.get('/analytics')
+        self.assertEqual(response.status_code, 200)
+
+    # Test that the listFiles view is responding to POST
+    def testListFilesPOST(self):
+        data = {
+            'token': 'ValidToken',
+            'path': '/',
+            'bucket': 'myBucket',
+        }
+        response = self.client.post('/api/listFiles',params=data)
+        self.assertEqual(response.status_code, 200)
+
+    
